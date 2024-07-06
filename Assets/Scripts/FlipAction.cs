75291a7b54793a1f46e4adaae33c41c6696b6c28
kiri10n <kiritohaze@gmail.com>
2024-07-06 15:03:09 +0900

    コマの裏返し時間をフレームではなく秒で制御

--- Assets/Scripts/FlipAction.cs
+++ Assets/Scripts/FlipAction.cs
@@ -7,22 +7,34 @@ public class FlipAction : MonoBehaviour
 {
     //左からi番目、上からj番目の駒を裏返す
     //GameObject[] flipPieces:今から裏返そうとしている駒たち
-    public bool FlipPiece(List<GameObject> flipPieces, int frame, int startFrame, int flipFramePerPiece)
+    public bool FlipPiece(List<GameObject> flipPieces, float presentTime, float startTime, float flipTimePerPiece)
     {
+        // 裏返す駒の数
         int flipPiecesNum = flipPieces.Count;
-        int flippingPieceIndex = (frame - startFrame) / flipFramePerPiece;
-        if (flippingPieceIndex >= flipPiecesNum) return true;
-        int startFrameOfFlippingPiece = startFrame + flippingPieceIndex * flipFramePerPiece;
-        int endFrameOfFlippingPiece = startFrameOfFlippingPiece + flipFramePerPiece;
+        float endTime = startTime + flipTimePerPiece * flipPiecesNum;
+        // すべての裏返しが終わったらtrueを返す
+        if (presentTime >= endTime)
+        {
+            return true;
+        }
+
+        // 実際に裏返す駒の番号
+        int flippingPieceIndex = Mathf.FloorToInt((presentTime - startTime) / flipTimePerPiece);
+        // 裏返す駒の、裏返し始める時間を取得
+        float startTimeOfFlippingPiece = startTime + flippingPieceIndex * flipTimePerPiece;
+        // 裏返す駒の、裏返し終わる時間を取得
+        float endTimeOfFlippingPiece = startTimeOfFlippingPiece + flipTimePerPiece;
+        // 裏返す駒のオブジェクトを取得
         GameObject flippingPiece = flipPieces[flippingPieceIndex];
+
         //駒のrotationを取得する
         Quaternion rotation = flippingPiece.transform.rotation;
         //クォータニオンからオイラー角への変換
         Vector3 rotationAngles = rotation.eulerAngles;
         //y軸中心に追加で180度回転
-        rotationAngles.y += 180.0f / flipFramePerPiece;
-        //rotationAngles += new Vector3(0.0f, 180.0f * (frame - startFrameOfFlippingPiece) / flipFramePerPiece, 0.0f);
-        if (frame >= endFrameOfFlippingPiece)
+        rotationAngles.y += 180.0f * Time.deltaTime / flipTimePerPiece;
+        // もしも裏返し終わっていたら、裏返し終わりの角度にする
+        if (presentTime >= endTimeOfFlippingPiece)
         {
             rotationAngles.y = Mathf.Round(rotationAngles.y / 10) * 10;
         }
@@ -31,21 +43,6 @@ public class FlipAction : MonoBehaviour
         //駒を回転
         flippingPiece.transform.rotation = rotation;
 
-        // //裏返すことになっている駒たちから、一つずつ取り出して裏返す
-        // foreach(GameObject piece in flipPieces)
-        // {
-        //     //駒のrotationを取得する
-        //     Quaternion rotation = piece.transform.rotation;
-        //     //クォータニオンからオイラー角への変換
-        //     Vector3 rotationAngles = rotation.eulerAngles;
-        //     //y軸中心に追加で180度回転
-        //     rotationAngles += new Vector3(0.0f, 180.0f, 0.0f);
-        //     //オイラー角からクォータニオンへの変換
-        //     rotation = Quaternion.Euler(rotationAngles);
-        //     //駒を回転
-        //     piece.transform.rotation = rotation;
-
-        // }
         return false;
     }
 
--- Assets/Scripts/GameDirector.cs
+++ Assets/Scripts/GameDirector.cs
@@ -15,8 +15,8 @@ public class GameDirector : MonoBehaviour
     bool endFrag = false; //ゲームの終了フラグ
     public int mode = 0;
     int next = 0; //next=0:どちらでもない、next=1:プレイヤー、next=2:COM
-    int flipTime = 120; //裏返りの時間
-    int flipStartTime; //裏返りの開始時間
+    float flipStartTime; //裏返りの開始時間
+    float flipTimePerPiece = 0.3f; //裏返りの時間
 
     // Start is called before the first frame update
     void Start()
@@ -57,14 +57,14 @@ public class GameDirector : MonoBehaviour
                 //プレイヤーのターン
                 //プレイヤーが駒を置いたらmodeを一つ進める
                 if(generatorComponent.Playing()){
-                    flipStartTime = Time.frameCount;
+                    flipStartTime = Time.time;
                     mode++;
                 }
                 break;
             case 2:
                 //プレイヤーの手による、駒の裏返りの時間
                 //裏返す駒を実際に裏返す
-                if(flipActionComponent.FlipPiece(controllComponent.flipPieces, Time.frameCount, flipStartTime, flipTime)){
+                if(flipActionComponent.FlipPiece(controllComponent.flipPieces, Time.time, flipStartTime, flipTimePerPiece)){
                     //裏返し終わったので、今から裏返そうとしている駒たちのリストの要素を削除
                     controllComponent.flipPieces.Clear();
                     //modeを一つ進める
@@ -97,14 +97,14 @@ public class GameDirector : MonoBehaviour
             case 4:
                 //相手のターン
                 if(controllComponent.ComTurn()){
-                    flipStartTime = Time.frameCount;
+                    flipStartTime = Time.time;
                     mode++;
                 }
                 break;
             case 5:
                 //相手の手による、駒の裏返りの時間
                 //裏返す駒を実際に裏返す
-                if(flipActionComponent.FlipPiece(controllComponent.flipPieces, Time.frameCount, flipStartTime, flipTime)){
+                if(flipActionComponent.FlipPiece(controllComponent.flipPieces, Time.time, flipStartTime, flipTimePerPiece)){
                     //裏返し終わったので、今から裏返そうとしている駒たちのリストの要素を削除
                     controllComponent.flipPieces.Clear();
                     //modeを一つ進める
